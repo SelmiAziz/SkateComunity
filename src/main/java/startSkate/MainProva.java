10052a00e8b91e9ba8a42c9ad60e625266830d80
SelmiAziz <Juventus201610@gmail.com>
2025-03-16 14:25:57 +0100

    resolving isssues

--- src/main/java/startSkate/MainProva.java
+++ src/main/java/startSkate/MainProva.java
@@ -14,27 +14,6 @@ import java.util.Properties;
 
 public class MainProva {
     public static void main(String[] args) {
-        //DbsConnector.getInstance().insertUser("messi", "ullo", "skate");
-        //DbsConnector.getInstance().insertProfile("skate",30,"costumer");
-        //DbsConnector.getInstance().insertProfile("BRo", 2, "costumer");
-
-        Properties p = new Properties();
-        try(FileInputStream databaseConfFile = new FileInputStream("src/main/resources/databasConf.properties")){
-            p.load(databaseConfFile);
-            String connectionUrl = p.getProperty("url");
-            String user = p.getProperty("user");
-            String password = p.getProperty("password");
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("HEllo");
-            //this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("bYE");
-        }catch(RuntimeException e){
-            //
-        }catch(ClassNotFoundException e){
-            //
-        }catch(IOException e){
-            //
-        }
-
+//
     }
 }
