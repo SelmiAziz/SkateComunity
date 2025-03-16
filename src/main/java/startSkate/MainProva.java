7c6bebaf907707bc4ff931579f5a9a5a28c7a4c8
SelmiAziz <Juventus201610@gmail.com>
2025-03-16 14:27:15 +0100

    resolving isssues

--- src/main/java/startSkate/MainProva.java
+++ src/main/java/startSkate/MainProva.java
@@ -1,16 +1,6 @@
 package startSkate;
 
-import login.User;
-import model.Costumer;
-import model.Organizer;
-import utils.DbsConnector;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Properties;
 
 public class MainProva {
     public static void main(String[] args) {
